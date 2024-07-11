b207dbb5f461091cdf32565a296fb3b2bc8815f5
Anthony Vladimir Gotay <39626593+VladlmirG@users.noreply.github.com>
2024-07-11 20:35:11 +0200

    Update CartModal.tsx

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -7,25 +7,22 @@ import { useWixClient } from "@/hooks/useWixClient";
 import { currentCart } from "@wix/ecom";
 
 const CartModal = () => {
-
   const wixClient = useWixClient();
   const { cart, isLoading, removeItem } = useCartStore();
 
   const handleCheckout = async () => {
     try {
-      const checkout =
-        await wixClient.currentCart.createCheckoutFromCurrentCart({
-          channelType: currentCart.ChannelType.WEB,
-        });
+      const checkout = await wixClient.currentCart.createCheckoutFromCurrentCart({
+        channelType: currentCart.ChannelType.WEB,
+      });
 
-      const { redirectSession } =
-        await wixClient.redirects.createRedirectSession({
-          ecomCheckout: { checkoutId: checkout.checkoutId },
-          callbacks: {
-            postFlowUrl: window.location.origin,
-            thankYouPageUrl: `${window.location.origin}/success`,
-          },
-        });
+      const { redirectSession } = await wixClient.redirects.createRedirectSession({
+        ecomCheckout: { checkoutId: checkout.checkoutId },
+        callbacks: {
+          postFlowUrl: window.location.origin,
+          thankYouPageUrl: `${window.location.origin}/success`,
+        },
+      });
 
       if (redirectSession?.fullUrl) {
         window.location.href = redirectSession.fullUrl;
@@ -49,12 +46,7 @@ const CartModal = () => {
               <div className="flex gap-4" key={item._id}>
                 {item.image && (
                   <Image
-                    src={wixMedia.getScaledToFillImageUrl(
-                      item.image,
-                      72,
-                      96,
-                      {}
-                    )}
+                    src={wixMedia.getScaledToFillImageUrl(item.image, 72, 96, {})}
                     alt=""
                     width={72}
                     height={96}
@@ -66,22 +58,16 @@ const CartModal = () => {
                   <div className="">
                     {/* TITLE */}
                     <div className="flex items-center justify-between gap-8">
-                      <h3 className="font-semibold">
-                        {item.productName?.original}
-                      </h3>
+                      <h3 className="font-semibold">{item.productName?.original}</h3>
                       <div className="p-1 rounded-sm flex items-center gap-2">
                         {item.quantity && item.quantity > 1 && (
-                          <div className="text-xs text-green-500">
-                            {item.quantity} x{" "}
-                          </div>
+                          <div className="text-xs text-green-500">{item.quantity} x{" "}</div>
                         )}
                         HNL.{item.price?.amount}
                       </div>
                     </div>
                     {/* DESC */}
-                    <div className="text-sm text-gray-500">
-                      {item.availability?.status}
-                    </div>
+                    <div className="text-sm text-gray-500">{item.availability?.status}</div>
                   </div>
                   {/* BOTTOM */}
                   <div className="flex justify-between text-sm">
@@ -102,15 +88,13 @@ const CartModal = () => {
           <div className="">
             <div className="flex items-center justify-between font-semibold">
               <span className="">Subtotal</span>
-              <span className="">HNL.{cart.subtotal.amount}</span>
+              <span className="">HNL.{cart.subtotal?.amount || 0}</span>
             </div>
             <p className="text-gray-500 text-sm mt-2 mb-4">
-             Envío e impuestos calculados al finalizar la compra.
+              Envío e impuestos calculados al finalizar la compra.
             </p>
             <div className="flex justify-between text-sm">
-              <button className="rounded-md py-3 px-4 ring-1 ring-hovr">
-                Ver cesta
-              </button>
+              <button className="rounded-md py-3 px-4 ring-1 ring-hovr">Ver cesta</button>
               <button
                 className="rounded-md py-3 px-4 bg-button hover:bg-hovr transition duration-500 text-white disabled:cursor-not-allowed disabled:opacity-75"
                 disabled={isLoading}
