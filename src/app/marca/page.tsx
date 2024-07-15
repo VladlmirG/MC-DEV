21c668bd6c891495cd6fec559013bdbd3fd18c15
Anthony Vladimir Gotay <39626593+VladlmirG@users.noreply.github.com>
2024-07-15 22:44:04 +0200

    Update page.tsx

--- src/app/marca/page.tsx
+++ src/app/marca/page.tsx
@@ -5,15 +5,19 @@ const marca = () => {
     <div className='px-4 pt-24 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative'>
       <div className='flex flex-col md:flex-row items-center md:items-start'>
         <img 
-          src="/slider/img1.jpeg" 
+          src="/marca.jpg" 
           alt="About Us" 
           className='w-full md:w-1/2 lg:w-1/3 mb-4 md:mb-0 md:mr-8 rounded-lg'
         />
-        <div>
-          <h1 className='text-2xl font-bold mb-4'>Acerca de nosotros</h1>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed consequatur alias cupiditate, doloribus tempore repudiandae earum odio tempora dolorem! Ratione nostrum ipsam velit tempore est accusantium placeat cum distinctio ex, iure sint consequuntur odit expedita ducimus veritatis pariatur architecto quo fuga repellat, quidem facilis at eos aperiam! Libero, fugit sed praesentium magnam totam est eveniet quibusdam nihil ad consectetur adipisci amet nemo necessitatibus aliquam, harum fugiat dolores cum sint velit ipsam saepe sunt odit quod! Laborum odit ipsam odio saepe. Explicabo impedit sint nisi, sed consequatur reiciendis commodi modi dolorum dolore quam expedita cum magnam assumenda, reprehenderit deserunt quaerat vero porro? Aperiam debitis vero velit cumque molestiae nisi fugiat consectetur, quos quis rem, illum error cum tempore repellendus officia magnam explicabo, expedita provident? Eveniet quos accusamus deserunt iure impedit esse commodi voluptatibus error neque magnam quis culpa consectetur, explicabo nostrum tempora perspiciatis at sunt perferendis nam dignissimos cupiditate sed aperiam ipsam! Saepe fugiat, nisi, quod tempora in minima nihil quisquam corporis, natus aspernatur quia. Voluptatum animi tempore vel voluptas quidem porro perspiciatis magni culpa! Quaerat, iure voluptatum harum sunt sit corrupti animi ducimus neque quis quos, enim iste, illum hic praesentium facilis voluptas ipsa dolores consequuntur? Assumenda nam repudiandae ipsam.</p><br></br>
 
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, odio placeat. Officiis, illo dolorum vitae sit, veritatis dignissimos deserunt aliquid voluptas iusto natus, voluptatibus velit vero quas ipsum. Eaque dolor perspiciatis tempora earum amet architecto quis odit temporibus magni eligendi. Expedita aliquid explicabo veritatis fugiat dignissimos, beatae officia optio perspiciatis eaque. Quos, nam? Recusandae, obcaecati consequatur error illo mollitia distinctio, numquam sit sunt sint inventore vel aliquid labore sequi at. Expedita numquam necessitatibus eum? Animi asperiores laboriosam nam minima, ipsum nobis impedit deleniti, placeat delectus odit pariatur esse et quo exercitationem excepturi? Porro quisquam tempora, perspiciatis et at aspernatur deserunt.</p>
+        <div>
+          <h1 className='text-2xl font-bold mb-4'>Fundadora de Mc Clothes</h1>
+          <p className='text-l'>Mc Clothes  es una marca que ha evolucionado desde tops basicos  hasta hermosos vestidos de Gala. Inaugurado online en Febrero del 2020 desde la casa de nuestos entonces fundadores Marie Cantarero y Charles Walter  . Ahora tenemos una comunidad de 36 mil seguidores y un equipo que satisface las necesidades de nuestros clientes.
+          </p><br></br>
+
+          <p className='text-l'>Desde 15 de Febrero del 2020 el 100 % de las prendas son elaboradas en Honduras  con mucho amor, confeccionadas por costureras que les apasiona la moda; resultando prendas inspiradas para mujeres icónicas.</p><br></br>
+
+          <p className='text-l'>Actualmente su matriz queda en San Pedro Sula, Honduras  donde sigue siendo una empresa online, y presencial que cuenta con un sistema integrado para mejorar la experiencia de la compra, con entregas a nivel nacional</p>
         </div>
       </div>
     </div>
