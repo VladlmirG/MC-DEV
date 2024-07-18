cad70b4bff0c11453e8741d53fa7e774136de2b0
Anthony Vladimir Gotay <39626593+VladlmirG@users.noreply.github.com>
2024-07-18 21:32:37 +0200

    Add files via upload

--- src/app/devoluciones/page.tsx
+++ src/app/devoluciones/page.tsx
@@ -4,13 +4,41 @@ const devoluciones = () => {
   return (
     <div className='px-4 pt-24 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative'>
         <div>
-          <h1 className='text-2xl font-bold mb-4'>Cancelaciones, Cambios y Devoluciones</h1>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed consequatur alias cupiditate, doloribus tempore repudiandae earum odio tempora dolorem! Ratione nostrum ipsam velit tempore est accusantium placeat cum distinctio ex, iure sint consequuntur odit expedita ducimus veritatis pariatur architecto quo fuga repellat, quidem facilis at eos aperiam! Libero, fugit sed praesentium magnam totam est eveniet quibusdam nihil ad consectetur adipisci amet nemo necessitatibus aliquam, harum fugiat dolores cum sint velit ipsam saepe sunt odit quod! Laborum odit ipsam odio saepe. Explicabo impedit sint nisi, sed consequatur reiciendis commodi modi dolorum dolore quam expedita cum magnam assumenda, reprehenderit deserunt quaerat vero porro? Aperiam debitis vero velit cumque molestiae nisi fugiat consectetur, quos quis rem, illum error cum tempore repellendus officia magnam explicabo, expedita provident? Eveniet quos accusamus deserunt iure impedit esse commodi voluptatibus error neque magnam quis culpa consectetur, explicabo nostrum tempora perspiciatis at sunt perferendis nam dignissimos cupiditate sed aperiam ipsam! Saepe fugiat, nisi, quod tempora in minima nihil quisquam corporis, natus aspernatur quia. Voluptatum animi tempore vel voluptas quidem porro perspiciatis magni culpa! Quaerat, iure voluptatum harum sunt sit corrupti animi ducimus neque quis quos, enim iste, illum hic praesentium facilis voluptas ipsa dolores consequuntur? Assumenda nam repudiandae ipsam.</p><br></br>
+          <h1 className='text-2xl font-bold mb-4'>Cancelaciones, Cambios y Devoluciones</h1><br/><br/>
+          <p><strong>Honduras</strong></p><br/>
 
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, odio placeat. Officiis, illo dolorum vitae sit, veritatis dignissimos deserunt aliquid voluptas iusto natus, voluptatibus velit vero quas ipsum. Eaque dolor perspiciatis tempora earum amet architecto quis odit temporibus magni eligendi. Expedita aliquid explicabo veritatis fugiat dignissimos, beatae officia optio perspiciatis eaque. Quos, nam? Recusandae, obcaecati consequatur error illo mollitia distinctio, numquam sit sunt sint inventore vel aliquid labore sequi at. Expedita numquam necessitatibus eum? Animi asperiores laboriosam nam minima, ipsum nobis impedit deleniti, placeat delectus odit pariatur esse et quo exercitationem excepturi? Porro quisquam tempora, perspiciatis et at aspernatur deserunt.</p><br/><br/><br/>
+          <p>Una vez realizado y pagado el pedido, este solo podrá ser cancelado si solo si el envío no se realizó. Una vez enviado el producto no se puede cancelar el pedido.</p><br/><br/>
 
-          <h2 className='text-xl font-bold mb-4'>Subtitulo</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, odio placeat. Officiis, illo dolorum vitae sit, veritatis dignissimos deserunt aliquid voluptas iusto natus, voluptatibus velit vero quas ipsum. Eaque dolor perspiciatis tempora earum amet architecto quis odit temporibus magni eligendi. Expedita aliquid explicabo veritatis fugiat dignissimos, beatae officia optio perspiciatis eaque. Quos, nam? Recusandae, obcaecati consequatur error illo mollitia distinctio, numquam sit sunt sint inventore vel aliquid labore sequi at. Expedita numquam necessitatibus eum? Animi asperiores laboriosam nam minima, ipsum nobis impedit deleniti, placeat delectus odit pariatur esse et quo exercitationem excepturi? Porro quisquam tempora, perspiciatis et at aspernatur deserunt.</p>
+          <p>A veces las prendas no quedan bien, así que en Mc Clothes aceptamos con gusto los cambios/devoluciones de productos, el cual se deben realizar en un lapso de 72 horas laborables a partir de la fecha de entrega. Los cambios/devoluciones de productos realizadas por motorizado tienen un lapso de 72 horas laborables a partir de la fecha de entrega. Las prendas deben estar con las respectivas etiquetas de cartón.</p><br/><br/>
+
+          <p>Los productos para cambios/devoluciones deben estar en perfectas condiciones, sin olores, manchas y cualquier otra consideración que perjudique poder retornarla a perchas. Las prendas que no entran a cambio/devolución son aquellas que tienen contacto intimo como el short booty control, lencerías, trajes de baño, bodys y accesorios como pareos, bandanas, rizador de cabello, cinta para senos, aretes, pulseras, cadenas, etc.
+          </p><br/><br/>
+
+          <p><strong>Ten en cuenta que Mc clothes  no cubre los costos de envío de los cambios/devoluciones.</strong></p><br/><br/><br/>
+
+          <p>Para dar seguimiento al caso recomendamos usar el mismo medio en el cual realizaste tu pedido. Indicar si quieres una NOTA DE CREDITO o un cambio. Algunas prendas no son aptas para cambios ni devoluciones, pero no te preocupes, estos productos así lo indican en las descripciones o términos y condiciones de nuestra web. LOS PRODUCTOS EN OFERTA NO APLICAN CAMBIOS NI DEVOLUCIONES. Las demás prendas se pueden devolver para reembolso a través de una nota de crédito. Las notas de crédito son válidas solo en el sitio web. No asumimos ninguna responsabilidad si los envíos se pierden al ser devueltos a nuestro depósito.</p><br/><br/>
+
+          <p>¡MUY IMPORTANTE! Si existe un error de nuestra parte en el envío de su pedido. Será nuestra responsabilidad asumir con TODOS LOS GASTOS DE ENVIOS. Para más información puede contactarnos a través de WhatsApp.
+          </p><br/><br/><br/>
+
+
+          <p><strong>POLÍTICAS DE DEVOLUCIÓN Y CANCELACIÓN EN PUNTO FÍSICO</strong></p><br/><br/>
+
+          <p>Las compras que son realizadas en el punto físico se regirán bajo cambio/devolución en un lapso de 48 horas de haber realizado la compra, presentando la respectiva factura.
+          </p><br/><br/>
+
+          <p>Los productos para cambios/devoluciones deben estar en perfectas condiciones, sin olores, manchas y cualquier otra consideración que perjudique poder retornarla a perchas. Las prendas que no entran a cambio/devolución son aquellas que tienen contacto íntimo como el short booty control, lencerías, trajes de baño, bodys 
+          Las devoluciones de productos se convertirán en NOTA DE CRÉDITO utilizable en cualquier momento con fecha de vencimiento de un año calendario a partir del día que es generada. Así mismo una vez hecho el cambio de la NOTA DE CRÉDITO contará con una última vez para realizar devoluciones.
+          </p><br/><br/>
+          
+          
+          <p><strong>USA</strong></p><br/><br/>
+
+          <p>Una vez realizado y pagado el pedido, este solo podrá ser cancelado si solo si el envío no se realizó. Una vez enviado el producto no se puede cancelar el pedido.</p><br/><br/>
+
+          <p>Actualmente en Honduras se complica el proceso de Cambios/Devoluciones de los productos de Envíos a Estados unidos debido que existen recargos del sistema de Aduanas de Honduras, pagos arancelarios y gastos de Courier porque al devolvernos el producto entraría como Importación de productos. Por eso es importante que revises bien la talla y las descripciones antes de realizar tu pedido ya que NO SE REALIZAN CAMBIOS NI DEVOLUCIONES EN ENVIOS Para estados unidos.</p><br/><br/>
+
+          <p>¡MUY IMPORTANTE! Si existe un error de nuestra parte en el envío de su pedido. Será nuestra responsabilidad asumir con TODOS LOS GASTOS DE ENVIOS. Para más información puede contactarnos a través de WhatsApp.</p><br/><br/>
         </div>
       </div>
   );
